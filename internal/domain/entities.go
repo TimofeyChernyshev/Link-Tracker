60c3303caba53c33739fa2549f279f050bc42978
Timofey Chernyshev <timkachern@mail.ru>
2026-02-20 23:28:52 +0300

    feat: add message and response entities

---
+++ internal/domain/entities.go
@@ -0,0 +1,15 @@
+package domain
+
+// Message - сообщение, присылаемое боту
+type Message struct {
+	Text      string
+	ChatID    int64
+	Username  string
+	MessageID int
+}
+
+// Response - ответ бота
+type Response struct {
+	Text   string
+	ChatID int64
+}
