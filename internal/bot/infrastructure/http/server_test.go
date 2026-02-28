c2221798d32111635f69f34acf447fa6b2674e58
Timofey Chernyshev <timkachern@mail.ru>
2026-02-28 20:35:40 +0300

    feat: add server for bot to handler post /updates

---
+++ internal/bot/infrastructure/http/server_test.go
@@ -0,0 +1,55 @@
+package bot_server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/domain"
+)
+
+func TestUpdates_OK(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sender := NewMockSender(ctrl)
+
+	s := NewServer(sender, "0")
+
+	reqBody := `{
+		"id": 1,
+		"url": "https://url",
+		"description": "updated",
+		"tgChatIds": [10, 20]
+	}`
+	sender.EXPECT().SendMessage(&domain.Response{Text: "updated", ChatID: 10})
+	sender.EXPECT().SendMessage(&domain.Response{Text: "updated", ChatID: 20})
+
+	req := httptest.NewRequest(http.MethodPost, "/updates", strings.NewReader(reqBody))
+	w := httptest.NewRecorder()
+
+	s.srv.Handler.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestUpdates_BadRequest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	sender := NewMockSender(ctrl)
+
+	s := NewServer(sender, "0")
+
+	reqBody := `{
+		"id": 
+	}`
+
+	req := httptest.NewRequest(http.MethodPost, "/updates", strings.NewReader(reqBody))
+	w := httptest.NewRecorder()
+
+	s.srv.Handler.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusBadRequest, w.Code)
+}
