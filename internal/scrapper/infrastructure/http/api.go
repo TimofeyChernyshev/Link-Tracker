e1e2f3fba617593323e3e1e08abbdd757d1d490e
Timofey Chernyshev <timkachern@mail.ru>
2026-03-01 16:44:46 +0300

    feat: implement server for scrapper

---
+++ internal/scrapper/infrastructure/http/api.go
@@ -0,0 +1,31 @@
+package scrapper_server
+
+type LinkResponse struct {
+	Id      int64    `json:"id"`
+	Url     string   `json:"url"`
+	Tags    []string `json:"tags"`
+	Filters []string `json:"filters"`
+}
+
+type ApiErrorResponse struct {
+	Description      string   `json:"description"`
+	Code             string   `json:"code"`
+	ExceptionName    string   `json:"exceptionName,omitempty"`
+	ExceptionMessage string   `json:"exceptionMessage,omitempty"`
+	Stacktrace       []string `json:"stacktrace,omitempty"`
+}
+
+type AddLinkRequest struct {
+	Link    string   `json:"link"`
+	Tags    []string `json:"tags"`
+	Filters []string `json:"filters"`
+}
+
+type ListLinksResponse struct {
+	Links []LinkResponse `json:"links"`
+	Size  int32          `json:"size"`
+}
+
+type RemoveLinkRequest struct {
+	Link string `json:"link"`
+}
