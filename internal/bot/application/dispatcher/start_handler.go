e5ef2a2239a2eabcdb8d160f939aa2daaf6e5ae9
Timofey Chernyshev <timkachern@mail.ru>
2026-03-02 20:41:23 +0300

    feat: add chat registration for /start command

--- internal/bot/application/dispatcher/start_handler.go
+++ internal/bot/application/dispatcher/start_handler.go
@@ -1,21 +1,34 @@
 package dispatcher
 
 import (
+	"context"
 	"log/slog"
+	"time"
 
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/domain"
 )
 
-type StartHandler struct{}
+type StartHandler struct {
+	linkService LinkService
+}
 
-func NewStartHandler() *StartHandler {
-	return &StartHandler{}
+func NewStartHandler(l LinkService) *StartHandler {
+	return &StartHandler{linkService: l}
 }
 
 func (sh *StartHandler) Execute(msg *domain.Message) (*domain.Response, bool, error) {
 	slog.Info("start requested", "chatID", msg.ChatID)
 
 	text := "Добро пожаловать, " + msg.Username + "!\nИспользуйте /help, чтобы посмотреть доступные команды"
+
+	context, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := sh.linkService.RegisterChat(context, msg.ChatID)
+	if err != nil {
+		slog.Error("start error", "error", err)
+		return nil, true, err
+	}
 
 	return &domain.Response{
 		Text:   text,
