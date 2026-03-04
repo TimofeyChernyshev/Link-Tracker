eedaaa580276106e8f52f5e8697d05dde65f2f58
Timofey Chernyshev <timkachern@mail.ru>
2026-03-04 21:17:44 +0300

    feat: update storage to work with urls first

--- internal/scrapper/infrastructure/storage/storage.go
+++ internal/scrapper/infrastructure/storage/storage.go
@@ -4,18 +4,23 @@ import (
 	"errors"
 	"log/slog"
 	"sync"
+	"time"
 
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/scrapper/domain"
 )
 
 type Storage struct {
-	mu        sync.RWMutex
-	userLinks map[int64]map[string]domain.Link // chatID -> map[url]Link
+	mu              sync.RWMutex
+	registeredChats map[int64]struct{}
+	links           map[string]*domain.Link       // url -> Link
+	subs            map[string]map[int64]struct{} // url -> chatID
 }
 
 func New() *Storage {
 	return &Storage{
-		userLinks: make(map[int64]map[string]domain.Link),
+		registeredChats: make(map[int64]struct{}),
+		links:           make(map[string]*domain.Link),
+		subs:            make(map[string]map[int64]struct{}),
 	}
 }
 
@@ -23,56 +28,82 @@ func (s *Storage) RegisterChat(chatId int64) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.userLinks[chatId] = make(map[string]domain.Link)
+	s.registeredChats[chatId] = struct{}{}
 }
 
 func (s *Storage) DeleteChat(chatId int64) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	delete(s.userLinks, chatId)
+	delete(s.registeredChats, chatId)
+
+	for url, set := range s.subs {
+		delete(set, chatId)
+
+		if len(set) == 0 {
+			delete(s.subs, url)
+			delete(s.links, url)
+		}
+	}
 }
 
 func (s *Storage) ChatExists(chatId int64) bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if _, exists := s.userLinks[chatId]; exists {
-		return true
-	} else {
-		return false
-	}
+	_, ok := s.registeredChats[chatId]
+
+	return ok
 }
 
 func (s *Storage) AddLink(chatId int64, url string, tags []string) (domain.Link, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if _, exists := s.userLinks[chatId][url]; exists {
+	link, exists := s.links[url]
+	if !exists {
+		link = &domain.Link{
+			URL:       url,
+			Tags:      tags,
+			UpdatedAt: time.Now(),
+		}
+
+		s.links[url] = link
+	}
+
+	if _, ok := s.subs[url]; !ok {
+		s.subs[url] = make(map[int64]struct{})
+	}
+
+	if _, ok := s.subs[url][chatId]; ok {
 		slog.Warn("link already tracked", "chatID", chatId, "url", url)
 		return domain.Link{}, errors.New("link already tracked")
 	}
 
-	link := domain.Link{URL: url, Tags: tags}
-	s.userLinks[chatId][url] = link
+	s.subs[url][chatId] = struct{}{}
 
-	return link, nil
+	return *link, nil
 }
 
 func (s *Storage) RemoveLink(chatId int64, url string) (domain.Link, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	userLinks := s.userLinks[chatId]
-
-	removedLink, exists := userLinks[url]
-	if !exists {
-		slog.Warn("link not found", "chatID", chatId, "url", url)
+	set, ok := s.subs[url]
+	if !ok {
 		return domain.Link{}, errors.New("link not found")
 	}
-	delete(userLinks, url)
 
-	return removedLink, nil
+	delete(set, chatId)
+
+	link := s.links[url]
+
+	if len(set) == 0 {
+		delete(s.subs, url)
+		delete(s.links, url)
+	}
+
+	return *link, nil
 }
 
 func (s *Storage) GetLinks(chatID int64) []domain.Link {
@@ -80,10 +111,46 @@ func (s *Storage) GetLinks(chatID int64) []domain.Link {
 	defer s.mu.RUnlock()
 
 	var result []domain.Link
-	if userLinks, exists := s.userLinks[chatID]; exists {
-		for _, link := range userLinks {
-			result = append(result, link)
+
+	for url, set := range s.subs {
+		if _, ok := set[chatID]; ok {
+			result = append(result, *s.links[url])
 		}
 	}
+
 	return result
+}
+
+func (s *Storage) GetAllLinks() []domain.Link {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	res := make([]domain.Link, 0, len(s.links))
+	for _, l := range s.links {
+		res = append(res, *l)
+	}
+
+	return res
+}
+
+func (s *Storage) GetSubscribers(url string) []int64 {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	set := s.subs[url]
+
+	res := make([]int64, 0, len(set))
+	for id := range set {
+		res = append(res, id)
+	}
+	return res
+}
+
+func (s *Storage) UpdateTimestamp(url string, t time.Time) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if l, ok := s.links[url]; ok {
+		l.UpdatedAt = t
+	}
 }
--- internal/scrapper/infrastructure/storage/storage_test.go
+++ internal/scrapper/infrastructure/storage/storage_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/scrapper/domain"
 )
 
 func TestRegisterAndExists(t *testing.T) {
@@ -58,7 +59,6 @@ func TestAddLink(t *testing.T) {
 func TestAddLink_Duplicate(t *testing.T) {
 	s := New()
 	chatID := int64(1)
-	s.RegisterChat(chatID)
 
 	_, err := s.AddLink(chatID, "url1", nil)
 	require.NoError(t, err)
@@ -71,7 +71,6 @@ func TestAddLink_Duplicate(t *testing.T) {
 func TestRemoveLink(t *testing.T) {
 	s := New()
 	chatID := int64(1)
-	s.RegisterChat(chatID)
 
 	const n = 10
 
@@ -104,4 +103,40 @@ func TestRemoveLink_NotFound(t *testing.T) {
 	_, err := s.RemoveLink(chatID, "url1")
 	require.Error(t, err)
 	assert.Equal(t, "link not found", err.Error())
+}
+
+func TestGetAllLinks(t *testing.T) {
+	s := New()
+	s.AddLink(1, "123", []string{"tag"})
+	s.AddLink(1, "256", []string{"tag"})
+	s.AddLink(2, "123", []string{"tag"})
+	s.AddLink(5, "678", []string{"tag"})
+
+	expectedLinks := []domain.Link{
+		{URL: "123", Tags: []string{"tag"}},
+		{URL: "256", Tags: []string{"tag"}},
+		{URL: "678", Tags: []string{"tag"}},
+	}
+
+	links := s.GetAllLinks()
+
+	require.Len(t, links, 3)
+
+	assert.Equal(t, expectedLinks[0].URL, links[0].URL)
+	assert.Equal(t, expectedLinks[1].URL, links[1].URL)
+	assert.Equal(t, expectedLinks[2].URL, links[2].URL)
+}
+
+func TestGetSubscribers(t *testing.T) {
+	s := New()
+	s.AddLink(1, "123", []string{"tag"})
+	s.AddLink(1, "256", []string{"tag"})
+	s.AddLink(2, "123", []string{"tag"})
+	s.AddLink(5, "678", []string{"tag"})
+
+	expectedSubs := []int64{1, 2}
+
+	subs := s.GetSubscribers("123")
+
+	assert.ElementsMatch(t, expectedSubs, subs)
 }
