f947e3b4e1933d54e5cb9b02bbc1ac15b2d9b6ca
Timofey Chernyshev <timkachern@mail.ru>
2026-03-23 06:51:43 +0300

    refactor: remove bot service, because dispatcher already implement it

--- internal/bot/application/service/interfaces.go
+++
@@ -1,7 +0,0 @@
-package service
-
-import "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/domain"
-
-type Bot interface {
-	SendMessage(response *domain.Response)
-}
