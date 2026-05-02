5932c2b8665e96723b1353e8285095a7fdf6f0c1
Timofey Chernyshev <timkachern@mail.ru>
2026-05-02 08:02:59 +0300

    chore: provide explanation why disable musttag lint

--- internal/scrapper/infrastructure/cache/valkey.go
+++ internal/scrapper/infrastructure/cache/valkey.go
@@ -64,10 +64,9 @@ func (c *ValkeyClient) GetLinks(ctx context.Context, chatID int64) ([]domain.Lin
 		return nil, fmt.Errorf("failed to get from cache: %w", err)
 	}
 
-	// Для кэша быстрее использовать сразу доменную модель
-	// иначе придется создавать промежуточную и переносить данные, на это уходит много ресурсов
-	//nolint:musttag
 	var links []domain.Link
+
+	//nolint:musttag // Для кэша быстрее использовать сразу доменную модель иначе придется создавать промежуточную и переносить данные, на это уходит много ресурсов
 	if err = json.Unmarshal(data, &links); err != nil {
 		return nil, fmt.Errorf("cannot unmarshal data: %w", err)
 	}
@@ -79,9 +78,7 @@ func (c *ValkeyClient) GetLinks(ctx context.Context, chatID int64) ([]domain.Lin
 func (c *ValkeyClient) SetLinks(ctx context.Context, chatID int64, links []domain.Link) error {
 	key := c.getKey(chatID)
 
-	// Для кэша быстрее использовать сразу доменную модель
-	// иначе придется создавать промежуточную и переносить данные, на это уходит много ресурсов
-	//nolint:musttag
+	//nolint:musttag // Для кэша быстрее использовать сразу доменную модель иначе придется создавать промежуточную и переносить данные, на это уходит много ресурсов
 	dataBytes, err := json.Marshal(links)
 	if err != nil {
 		return fmt.Errorf("cannot unmarshal data: %w", err)
