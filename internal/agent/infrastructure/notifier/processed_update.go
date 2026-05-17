4a26226380788fde29a680cd9ac05c31af0614ab
Timofey Chernyshev <timkachern@mail.ru>
2026-05-17 14:33:36 +0300

    feat: implement kafka notifier for agent to send into link.processed-updates topic

---
+++ internal/agent/infrastructure/notifier/processed_update.go
@@ -0,0 +1,80 @@
+package kafkanotifier
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/agent/domain"
+)
+
+type KafkaNotifier struct {
+	writer *kafka.Writer
+	topic  string
+}
+
+func NewKafkaNotifier(topic, compression string, brokers []string, batchSize, requiredAcks int, batchTimeout time.Duration) *KafkaNotifier {
+	writer := &kafka.Writer{
+		Addr:         kafka.TCP(brokers...),
+		Topic:        topic,
+		Balancer:     &kafka.LeastBytes{},
+		BatchSize:    batchSize,
+		BatchTimeout: batchTimeout,
+		RequiredAcks: kafka.RequiredAcks(requiredAcks),
+		Compression:  getCompression(compression),
+	}
+
+	return &KafkaNotifier{
+		writer: writer,
+		topic:  topic,
+	}
+}
+
+func (n *KafkaNotifier) SendUpdate(ctx context.Context, upd domain.ProcessedUpdate) error {
+	data, err := json.Marshal(upd)
+	if err != nil {
+		slog.Error("cannot marshal update", "error", err)
+		return fmt.Errorf("cannot marshal update: %w", err)
+	}
+
+	msg := kafka.Message{
+		Key:   []byte(strconv.FormatInt(upd.ID, 10)),
+		Value: data,
+		Time:  time.Now(),
+	}
+
+	if err = n.writer.WriteMessages(ctx, msg); err != nil {
+		slog.Error("cannot send message to kafka", "error", err)
+		return fmt.Errorf("cannot send message: %w", err)
+	}
+
+	return nil
+}
+
+func (n *KafkaNotifier) Close() error {
+	err := n.writer.Close()
+	if err != nil {
+		return fmt.Errorf("cannot close kafka notifier: %w", err)
+	}
+
+	return nil
+}
+
+func getCompression(c string) kafka.Compression {
+	switch c {
+	case "gzip":
+		return kafka.Gzip
+	case "snappy":
+		return kafka.Snappy
+	case "lz4":
+		return kafka.Lz4
+	case "zstd":
+		return kafka.Zstd
+	default:
+		return kafka.Snappy
+	}
+}
