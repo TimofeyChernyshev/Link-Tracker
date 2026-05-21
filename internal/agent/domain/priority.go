225f2b61399e978f2f8ba95ade2c99c3a1e4117d
Timofey Chernyshev <timkachern@mail.ru>
2026-05-21 23:05:54 +0300

    feat: add priority comparing

--- internal/agent/domain/priority.go
+++ internal/agent/domain/priority.go
@@ -7,3 +7,15 @@ const (
 	MediumPriority = "MEDIUM"
 	LowPriority    = "LOW"
 )
+
+var (
+	priorityOrder = map[Priority]int{
+		HighPriority:   3,
+		MediumPriority: 2,
+		LowPriority:    1,
+	}
+)
+
+func (p Priority) IsHigher(other Priority) bool {
+	return priorityOrder[p] > priorityOrder[other]
+}
